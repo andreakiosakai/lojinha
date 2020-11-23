9a1e933fd8e8f227ab2b0d46a87a91b34fc0bd9d
André Akio Furuko Sakai <andreakiosakai@gmail.com>
2020-11-23 19:33:13 -0300

    Admin and Categories

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'created_at', 'modified_at']
+    search_fields = ['name','slug']
+    list_filter = ['created_at', 'modified_at']
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'category', 'created_at', 'modified_at']
+    search_fields = ['name','slug', 'category__name']    
+    list_filter = ['created_at', 'modified_at']
+
+    
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- catalog/models.py
+++ catalog/models.py
@@ -13,6 +13,9 @@ class Category(models.Model):
         verbose_name = 'Categoria'
         verbose_name_plural = 'Categorias'
         ordering = ['name']
+    
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -30,3 +33,6 @@ class Product(models.Model):
         verbose_name = 'Produto'
         verbose_name_plural = 'Produtos'
         ordering = ['name']
+
+    def __str__(self):
+        return self.name
