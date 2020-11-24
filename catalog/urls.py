fb5a5f0096e7a979c9720f16ce80dfc4293ac038
André Akio Furuko Sakai <andreakiosakai@gmail.com>
2020-11-23 21:54:08 -0300

    Adding product list and categories

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include, re_path
 from . import views
 
 
 urlpatterns = [
-    path('', views.product_list, name='product_list' )
+    path('', views.product_list, name='product_list'),
+    re_path(r'^(?P<slug>\w+)/$', views.category, name='category'),
 ]
--- store/settings.py
+++ store/settings.py
@@ -15,8 +15,8 @@ import os
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'store.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
